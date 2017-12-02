c9d8bbcab601eb5e6868904bf043a918688d4606
Matthew Hunger <matt.hunger@gmail.com>
2017-12-02 17:40:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,15 @@
 
 class Shoe
 
-  attr_accessor
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
   end
 
 end
